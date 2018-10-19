a7f30bd107d9f7957dc57cc3f91149f120039bc6
gabe1314 <gabriel.sandoval1314@outlook.com>
2018-10-19 15:26:17 -0400

    Terraform_Automation-Error.ps1 Commit

---
+++ PSScripts/Terraform_Automation-Error.ps1
@@ -0,0 +1,32 @@
+﻿Start-Transcript -path "E:\terraform\logs\terraform_Plan.log" -append
+
+Write-Host "Terraform init Starting" 
+
+    Start-Job -Name Job1 -ScriptBlock {terraform init -lock=false -input=false}
+        
+        Wait-Job -Name Job1
+
+Write-Host "Terraform Init completed Successful" 
+
+
+Write-Host "Terraform Automation Starting" 
+    
+    if(Get-Content E:\terraform\logs\terraform_Plan.log -Tail 3 | Select-String -Pattern "The process cannot access the file because another process has locked" -AllMatches)
+    
+    {
+ Stop-Process -Name terraform
+
+{
+
+else
+}
+    
+    Write-Host "Terraform Planning will now start" 
+        terraform plan -lock=false -out=tfplan -input=false
+
+    Write-Host "Waiting to Apply"
+        Start-Sleep -Seconds 15    
+        terraform apply tfplan -lock=false -input=false
+}
+    Stop-Transcript
+echo "$(Get-Date -format T) - Terraform Updated"
