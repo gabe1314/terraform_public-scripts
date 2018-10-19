61896a678610b8443286239859ecc5208cfcdd50
gabe1314 <gabriel.sandoval1314@outlook.com>
2018-10-19 14:29:50 -0400

    Second Commit

---
+++ PSScripts/terraform_update.ps1
@@ -0,0 +1,18 @@
+﻿
+$BasePath = "F:\SDS\terraform\scripts\environment\C33SDSAppStIP-uat\scripts"
+
+
+Write-Host "Terraform Update starting"
+
+if (Test-Path ($BasePath + "terraforminit.ps1"))
+{
+    Write-Host "Terraform init Starting"
+    Invoke-Expression -command ($BasePath + "terraforminit.ps1")
+}
+else
+{
+    Write-Host "Terraform init Failed"
+    $errorType = "Init fail"
+}
+
+ 
