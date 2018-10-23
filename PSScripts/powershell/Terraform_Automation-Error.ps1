001ffc4a0c6f2e62f42f0f5120167b902d9315bf
gabe1314 <gabriel.sandoval1314@outlook.com>
2018-10-22 23:49:43 -0400

    Terraform Powershell Automation - Stop process Working

--- PSScripts/powershell/Terraform_Automation-Error.ps1
+++ PSScripts/powershell/Terraform_Automation-Error.ps1
@@ -1,9 +1,6 @@
 ﻿Start-Transcript -path "E:\terraform\logs\terraform_Plan.log" -append
 
-
-
-
-Write-Host "Terraform Automation Starting" 
+Write-Host "Terraform init Starting" 
 
     Start-Job -Name Job1 -ScriptBlock {terraform init -lock=false -input=false}
         
--- PSScripts/powershell/Terraform_Automation-Error2.ps1
+++ PSScripts/powershell/Terraform_Automation-Error2.ps1
@@ -1,22 +1,50 @@
-﻿Start-Transcript -path "E:\terraform\logs\terraform_Plan.log" -append
+﻿$BasePath = "F:\SDS\terraform\environment\C33SDSWSStIP-uat\"
+$errorType = "none"
 
 
+Start-Transcript -path "E:\terraform\logs\terraform_Plan.log" -append
+
+Stop-Process -Name terraform
 
 
-Write-Host "Terraform Automation Starting" 
+if($errorType -eq "none")
+{
+
+   Write-Host "Terraform init Starting" 
 
     Start-Job -Name Job1 -ScriptBlock {terraform init -lock=false -input=false}
         
         Wait-Job -Name Job1
 
 Write-Host "Terraform Init completed Successful" 
+}
 
 
-Write-Host "Terraform Automation Starting" 
+if(Get-Content E:\terraform\logs\terraform_Plan.log -Tail 3 | Select-String -Pattern "The process cannot access the file because another process has locked" -AllMatches)
+    
+    {
+ Stop-Process -Name terraform
+
+}
+ 
+
+   Start-Job -Name Job1 -ScriptBlock {terraform init -lock=false -input=false
+        
+        Wait-Job -Name Job1
+
+Write-Host "Terraform Init completed Successful" }
+
+    
+{
+
+else
+}
+
+Write-Host "Terraform planning" 
     
     if(Get-Content E:\terraform\logs\terraform_Plan.log -Tail 3 | Select-String -Pattern "The process cannot access the file because another process has locked" -AllMatches)
     
-    {
+    
  Stop-Process -Name terraform
 
 {
@@ -30,6 +58,6 @@ else
     Write-Host "Waiting to Apply"
         Start-Sleep -Seconds 15    
         terraform apply tfplan -lock=false -input=false
-}
+
     Stop-Transcript
 echo "$(Get-Date -format T) - Terraform Updated"
